25904355a7a1776a4fa79df9c133d624fe690b3a
Marcus Rands <marcus.rands@scholarpack.com>
2020-08-20 19:12:28 +0100

    Set up db. Add controller and views for reflections.

---
+++ app/controllers/reflections_controller.rb
@@ -0,0 +1,21 @@
+class ReflectionsController < ApplicationController
+    def show
+        @reflection = Reflection.find(params[:id])
+    end
+    
+    def new
+    end
+
+    def create
+        @reflection = Reflection.new(reflection_params)
+ 
+        @reflection.save
+        redirect_to @reflection
+    end
+
+    private
+    
+    def reflection_params
+        params.require(:reflection).permit(:title, :text)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   
+  resources :reflections
+
   root 'welcome#index'
 end
