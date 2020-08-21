5fd9be00087562aa22088e2ac0acfbc5c1b11d6f
Marcus Rands <marcus.rands@scholarpack.com>
2020-08-21 11:20:53 +0100

    Add edit action for reflections. Add validation to reflection form.

--- app/controllers/reflections_controller.rb
+++ app/controllers/reflections_controller.rb
@@ -2,23 +2,41 @@ class ReflectionsController < ApplicationController
     def index
         @reflections = Reflection.all
     end
-    
+
     def show
         @reflection = Reflection.find(params[:id])
     end
-    
+
     def new
+        @reflection = Reflection.new
+    end
+
+    def edit
+        @reflection = Reflection.find(params[:id])
     end
 
     def create
         @reflection = Reflection.new(reflection_params)
- 
-        @reflection.save
-        redirect_to @reflection
+
+        if @reflection.save
+            redirect_to @reflection
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @reflection = Reflection.find(params[:id])
+
+        if @reflection.update(reflection_params)
+            redirect_to @reflection
+        else
+            render 'edit'
+        end
     end
 
     private
-    
+
     def reflection_params
         params.require(:reflection).permit(:title, :text)
     end
