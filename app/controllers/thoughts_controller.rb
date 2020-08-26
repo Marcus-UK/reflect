afdc00db1331ed3b5461062ff591df66fbad8f9b
Marcus Rands <marcus.rands@scholarpack.com>
2020-08-26 10:22:07 +0100

    Add function to delete thoughts on reflections.

--- app/controllers/thoughts_controller.rb
+++ app/controllers/thoughts_controller.rb
@@ -4,9 +4,17 @@ class ThoughtsController < ApplicationController
         @thought = @reflection.thoughts.create(thought_params)
         redirect_to reflection_path(@reflection)
       end
+
+    def destroy
+    @reflection = Reflection.find(params[:reflection_id])
+    @thought = @reflection.thoughts.find(params[:id])
+    @thought.destroy
+    redirect_to reflection_path(@reflection)
+    end  
      
-      private
-        def thought_params
-          params.require(:thought).permit(:thoughter, :body)
-        end
+    private
+
+    def thought_params
+      params.require(:thought).permit(:thoughter, :body)
+    end
 end
--- app/models/reflection.rb
+++ app/models/reflection.rb
@@ -1,5 +1,5 @@
 class Reflection < ApplicationRecord
-    has_many :thoughts
+    has_many :thoughts, dependent: :destroy
     validates :title, presence: true,
                     length: { minimum: 5 }
 end
